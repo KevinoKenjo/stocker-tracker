6df83e3ebfe08e244ac65f4594df6e18539c3a8e
KevinoKenjo <kenjokevinodzory@gmail.com>
2025-04-06 21:19:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import NewsFeed from "./components/NewsFeed";
+
 function App() {
   return (
     <div>
       <h1>Bienvenue sur Stocker Tracker</h1>
       <p>Application de suivi des marchés boursiers</p>
+
+      <NewsFeed />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+ 
