5aa68083f988d57e6496b367fbeffcaef4b90ac8
KevinoKenjo <kenjokevinodzory@gmail.com>
2025-04-06 20:33:48 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur Stocker Tracker</h1>
+      <p>Application de suivi des marchés boursiers</p>
+    </div>
+  )
+}
+
+export default App
